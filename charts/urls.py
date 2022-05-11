bf614608cbb972d937b0a444fa74833913077652
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-11 15:38:49 -0300

    😉 Drop down escolha de moeda e período

--- charts/urls.py
+++ charts/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'cotacoes'
 urlpatterns = [
     # path('', views.home, name='home'),
-    path('delta', views.home, name='home'),
+    path('', views.home, name='home'),
+    # path('delta/<par_moedas>/<int:dias>', views.home, name='home'),
+
 ]
--- charts/views.py
+++ charts/views.py
@@ -7,16 +7,20 @@ import charts.payoff_utils as pu
 
 
 def home(request):
-    par_moedas = 'USD-BRL' 
-    qtd_dias = '60'
-    dict_cotacoes = lc.cria_lista_de_cotacoes_por_qtd_dias(par_moedas, qtd_dias)
-    # print(dict_cotacoes)
-    # context_dict = dict_cotacoes
-    dict_cotacoes['par_moedas'] = 'USD-BRL' 
-    dict_cotacoes['qtd_dias'] = '60'
+    # valores padrao caso não receba parametros GET
+    par_moedas = 'BTC-BRL'
+    dias = '90'
+    # par_moedas = 
+    if 'par_moedas_form' in request.GET:
+        par_moedas = request.GET['par_moedas_form']
+    if 'dias_form' in request.GET:
+        dias = request.GET['dias_form']
+
+    dict_cotacoes = lc.cria_lista_de_cotacoes_por_qtd_dias(par_moedas, dias)
+
+    dict_cotacoes['par_moedas'] = par_moedas
+    dict_cotacoes['qtd_dias'] = dias
 
     return render(request, 'charts/index.html', context = dict_cotacoes)
+
     
-    
-    # from charts.models import Ativo
-    # p = Ativo.objects.all().order_by('-id')[:10]
